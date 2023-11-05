4e1b0984d0e976da9a52c13daae6a361e7f768e1
YEAHx4 <developerstarlight@gmail.com>
2023-11-05 13:57:00 +0900

    render map

--- index.js
+++ index.js
@@ -4,10 +4,23 @@ let fpsCounter
 const size = 32
 const w = 10
 const h = 20
+const map = []
+
+const white = '#ffffff'
+
+function forEach(func) {
+    for (let i = 0; i < w; i++)
+        for (let j = 0; j < h; j++)
+            func(i, j)
+}
 
 function init() {
     canvas = document.getElementById('canvas')
     fpsCounter = document.getElementById('fps')
+    for (let i = 0; i < w; i++)
+        map.push([])
+    forEach((x, y) => map[x].push(new Cell(x, y, true, white)))
+
     requestAnimationFrame(draw)
 }
 
@@ -43,12 +56,43 @@ function calculateFps() {
     }
 }
 
+class Cell {
+    x
+    y
+    isAir
+    color
+
+    constructor(x, y, isAir, color) {
+        this.x = x
+        this.y = y
+        this.isAir = isAir
+        this.color = color
+    }
+
+    isBottom() {
+        if (this.y >= h - 1)
+            return true
+        else if (!map[this.x][this.y + 1].isAir)
+            return true
+
+        return false
+    }
+}
+
+function renderMap(ctx) {
+    forEach((x, y) => {
+        ctx.fillStyle = map[x][y].color
+        ctx.fillRect(x * size + 1, y * size + 1, size - 1, size - 1)
+    })
+}
+
 function draw() {
     const ctx = canvas.getContext('2d')
     ctx.imageSmoothingEnabled = false
 
     clearCanvas(ctx)
     drawGrid(ctx)
+    renderMap(ctx)
 
     calculateFps()
     requestAnimationFrame(draw)
